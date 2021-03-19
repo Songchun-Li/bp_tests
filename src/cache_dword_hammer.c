5f74f65ef2167b495f65dc1961840a394a3c6d76
Songchun <rafael.fc.lee@outlook.com>
2021-03-18 21:02:08 -0700

    Renaming variable

--- src/cache_dword_hammer.c
+++ src/cache_dword_hammer.c
@@ -9,22 +9,21 @@ int main(int argc, char** argv) {
     uint64_t *base_addr = (uint64_t *)(0x80100000 + (core_id << 14));
 
     uint64_t dcache_block_size = 128;
-    uint64_t scale_factor = dcache_block_size / 64; // pointer arithmetic 
-
+    uint64_t block_step = dcache_block_size / 64; // pointer arithmetic 
 
     uint64_t dcache_sets = 256;
     uint64_t dcache_assoc = 2;
     uint64_t total_block_cnt = dcache_sets*dcache_assoc*2; // multiply by 2 because we want eviction
 
     for (int i = 0; i < total_block_cnt; i++) {
-        uint64_t *addr = base_addr + i*scale_factor;
+        uint64_t *addr = base_addr + (i * block_step);
         uint64_t data = (uint64_t) addr | 0x0fff;
 
         *addr = data;
     }
 
     for (int i = 0; i < total_block_cnt; i++) {
-        uint64_t *addr = base_addr + i*scale_factor;
+        uint64_t *addr = base_addr + (i * block_step);
         uint64_t expected = (uint64_t) addr | 0x0fff;
         uint64_t actual = *addr;
 
